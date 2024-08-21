e82de988e883b69b343acdcdcf6adc9ad295bd00
Hallo.Q <dunkee182@gmail.com>
2024-08-21 17:34:54 +0800

    make nanogpt pip installable

---
+++ nanogpt/__init__.py
@@ -0,0 +1,6 @@
+from .model import LayerNorm
+from .model import CausalSelfAttention
+from .model import MLP
+from .model import Block
+from .model import GPTConfig
+from .model import GPT
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nanogpt",
+    version="0.2.0.0",
+    author="Hallo.Q",
+    author_email="dunkee182@gmail.com",
+    description="A simple Transformer.",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+    ],
+)
